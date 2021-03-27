a83ac56a95f89bc3fd4e6435e7988c6d6c2afb28
Kurban <bdobik@mail.ru>
2021-03-27 17:00:40 +0300

    task 14 DOM

--- javascript-task/script.js
+++ javascript-task/script.js
@@ -1,41 +1,38 @@
-//Создайте и добавьте с помощью JS между блоками one и three, div, внутри которого будет курсивный текст “dolor sit amet”;
-
-let div = document.createElement('div');
-let divOne = document.getElementById('one');
-divOne.after(div);
-divOne.nextElementSibling;
-divOne.nextElementSibling.append('dolor sit amet');
-divOne.nextElementSibling.classList.add('italic');
-
-//Добавьте для этого дива атрибут id со значением two.
-divOne.nextElementSibling.id = 'two';
-
-//Добавьте в начала списка элемент li с тектом html
-let ul = document.getElementsByTagName('ul')[0];
-let li = document.createElement('li');
-ul.prepend(li)
-li.innerHTML = 'html';
-
-//Добавьте в список элемент li с тектом js между элементами git и react
-let li2 = document.createElement('li');
-ul.lastElementChild.before(li2);
-li2.textContent = 'js';
-
-// Удалите div с классом “zero”
-let divZero = document.getElementsByClassName('zero')[0].remove()
-
-// Каждому из трех оставшихся дивов задайте свой фоновый цвет
-divOne.style.backgroundColor = 'orange';
-document.getElementById('two').style.backgroundColor = 'green';
-document.getElementById('three').style.backgroundColor = 'yellow';
-
-// Уберите маркеры у списка (свойство: `list-style: none;`)
-ul.classList.add('resetListStyle');
-
-// К диву с классом “main” добавьте класс “container”
-document.getElementsByClassName('main')[0].classList.add('container');
 
 
+let main = document.getElementById('main-post').firstElementChild;
+let mainTitle = main.firstElementChild.firstElementChild;
+
+let post1 = document.getElementById('post-1-tab');
+let post2 = document.getElementById('post-2-tab');
+let post3 = document.getElementById('post-3-tab');
+let post4 = document.getElementById('post-4-tab');
+
+post1.addEventListener('click', actPost);
+post2.addEventListener('click', actPost);
+post3.addEventListener('click', actPost);
+post4.addEventListener('click', actPost);
+
+post1.addEventListener('click', changeMain);
+post2.addEventListener('click', changeMain);
+post3.addEventListener('click', changeMain);
+post4.addEventListener('click', changeMain);
 
 
+function actPost() {
+    post1.classList.remove('active');
+    post2.classList.remove('active');
+    post3.classList.remove('active');
+    post4.classList.remove('active');
+    this.classList.add('active');
+}
+
+function changeMain() {
+    let img = this.firstElementChild.firstElementChild.firstElementChild.src;
+    let title = this.firstElementChild.lastElementChild.firstElementChild.innerHTML;
+    main.style.backgroundImage = `url(${img})`;
+    mainTitle.innerHTML = title;
+
+
+}
 
